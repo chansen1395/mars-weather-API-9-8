ff9d1f1c5f74aa9eb3efa54c771c8ba878deb32d
Connor Hansen <chansen13@georgefox.edu>
2021-09-08 09:28:26 -0700

    add basic initial files.

--- src/index.js
+++ src/index.js
@@ -2,28 +2,80 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './weather-service.js'
+
+function clearFields() {
+  $('#location').val("");
+  $('.showErrors').text("");
+  $('.showHumidity').text("");
+  $('.showTemp').text("");
+}
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
-
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    }
-
-    request.open("GET", url, true);
-    request.send();
-
-    function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-    }
+    let city = $('#location').val();
+    clearFields();
+    let promise = WeatherService.getWeather(city);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
   });
 });
+
+
+
+//************ OLD CODE *********
+// import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './css/styles.css';
+
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     let city = $('#location').val();
+//     $('#location').val("");
+
+// // New code begins here.
+
+//     let promise = new Promise(function(resolve, reject) {
+//       let request = new XMLHttpRequest();
+//       const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+//       request.onload = function() {
+//         if (this.status === 200) {
+//           resolve(request.response);
+//         } else {
+//           reject(request.response);
+//         }
+//       }
+//       request.open("GET", url, true);
+//       request.send();
+//     });
+
+//     promise.then(function(response) {
+//       const body = JSON.parse(response);
+//       $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+//       $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+//       $('.showErrors').text("");
+//     }, function(error) {
+//       $('.showErrors').text(`There was an error processing your request: ${error}`);
+//       $('.showHumidity').text("");
+//       $('.showTemp').text("");
+//     });
+//   });
+// });
+// **************************
+
+
+
+//     function getElements(response) {
+//       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+//       $('.showFarenheit').text(`The temperature in Farenheit is ${((response.main.temp - 273.15) * 9/5 + 32).toFixed(1)} degrees.`);
+//       $('.showDescription').text(`Description: ${response.weather[0].description}`);
+//     }
+//   });
+// });
